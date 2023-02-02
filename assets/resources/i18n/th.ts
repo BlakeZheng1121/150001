2d6d3ee418dccc02bddd6654213b086652477144
costang <costang@jumbogames.com.tw>
2023-02-02 09:19:18 +0800

    feat: 增加泰文語系的多國語系文字訊息

---
+++ assets/resources/i18n/th.ts
@@ -0,0 +1,13 @@
+
+const win = window as any;
+
+export const languages = {
+    // Data
+    loading: "กําลังเชื่อมต่อ...",
+};
+
+if (!win.languages) {
+    win.languages = {};
+}
+
+win.languages.th = languages;
