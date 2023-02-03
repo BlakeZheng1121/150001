856c45c2e6e9efdfd71d6286fcaeeb222e5a453e
costang <costang@jumbogames.com.tw>
2023-02-03 16:51:08 +0800

    fix: Release版本Mock清除錯誤
    1. 修正Build Release版本將mock移除時處理錯誤導致遺失一個分號(":")，後續i18n處理此json反序列時候卡死
    2. 庚改套件名稱，移除多餘變數，移除多餘hook

--- extensions/cocos-remove-mock/dist/hooks.js
+++ extensions/cocos-remove-mock/dist/hooks.js
@@ -12,22 +12,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.unload = exports.onAfterBuild = exports.onAfterCompressSettings = exports.onBeforeCompressSettings = exports.onBeforeBuildAssets = exports.onBeforeBuild = exports.load = exports.throwError = void 0;
+exports.unload = exports.onBeforeBuild = exports.throwError = void 0;
 const fs_1 = __importDefault(require("fs"));
 const path_1 = require("path");
-const PACKAGE_NAME = 'cocos-build-template';
+const PACKAGE_NAME = 'cocos-remove-mock';
 function log(...arg) {
     return console.log(`[${PACKAGE_NAME}] `, ...arg);
 }
-let allAssets = [];
 exports.throwError = true;
-function load() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log(`[${PACKAGE_NAME}] Load cocos plugin example in builder.`);
-        allAssets = yield Editor.Message.request('asset-db', 'query-assets');
-    });
-}
-exports.load = load;
 function onBeforeBuild(options) {
     return __awaiter(this, void 0, void 0, function* () {
         // Todo some thing
@@ -69,7 +61,7 @@ function removeReference() {
                     const mockLeftIndex = data.indexOf('{', mockStartIndex);
                     const mockRightIndex = data.indexOf('}', mockStartIndex);
                     data = data
-                        .slice(0, mockLeftIndex - 1)
+                        .slice(0, mockLeftIndex)
                         .concat('null')
                         .concat(data.slice(mockRightIndex + 1));
                     fs_1.default.writeFile(path, data, (err) => {
@@ -85,35 +77,6 @@ function removeReference() {
         });
     });
 }
-function onBeforeBuildAssets(options, result) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Todo some thing
-        log(`${PACKAGE_NAME}.webTestOption`, 'onBeforeBuildAssets');
-    });
-}
-exports.onBeforeBuildAssets = onBeforeBuildAssets;
-function onBeforeCompressSettings(options, result) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const pkgOptions = options.packages[PACKAGE_NAME];
-        if (pkgOptions.webTestOption) {
-            console.debug('webTestOption', true);
-        }
-        // Todo some thing
-        console.debug('get settings test', result);
-    });
-}
-exports.onBeforeCompressSettings = onBeforeCompressSettings;
-function onAfterCompressSettings(options, result) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Todo some thing
-        console.log('webTestOption', 'onAfterCompressSettings');
-    });
-}
-exports.onAfterCompressSettings = onAfterCompressSettings;
-function onAfterBuild(options, result) {
-    return __awaiter(this, void 0, void 0, function* () { });
-}
-exports.onAfterBuild = onAfterBuild;
 function unload() {
     console.log(`[${PACKAGE_NAME}] Unload cocos plugin example in builder.`);
 }
