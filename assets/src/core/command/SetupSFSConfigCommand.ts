dd302ba4eda17b69623a27ccf917d0dc573c3010
algernontu <algernontu@jumbogames.com.tw>
2023-06-12 13:23:09 +0800

    chore : 幣別顯示判斷修改
    JHS1030-45

--- assets/src/core/command/SetupSFSConfigCommand.ts
+++ assets/src/core/command/SetupSFSConfigCommand.ts
@@ -46,7 +46,7 @@ export class SetupSFSConfigCommand extends puremvc.SimpleCommand {
         let gameDataProxy: CoreGameDataProxy = this.getGameDataProxy();
         gameDataProxy.gameType = ticket['gameType'];
         gameDataProxy.machineType = ticket['machineType'];
-        gameDataProxy.currency = ticket['currency'];
+        gameDataProxy.currency = ticket['currencySystemName'];
         gameDataProxy.connectedTimeout = ticket['gameConnectionTimeout'];
         gameDataProxy.resLoadingTimeout = ticket['gameResourceTimeout'];
     }
