a6bb840eecbdb75cd65c5f6d169b5b94e3c7b168
chungen <chungen@jumbogames.com.tw>
2023-06-14 11:40:26 +0800

    feat: 新增大獎預告音效隨機撥放功能 fix: 串接大獎預告動畫
    JHS1030-34

--- assets/src/game/view/PrizePredictionView.ts
+++ assets/src/game/view/PrizePredictionView.ts
@@ -1,15 +1,19 @@
 import { _decorator, Component, Node, Prefab, CCString } from 'cc';
+import { TimeLineTool } from '../../../../extensions/timelinetool/assets/src/ta/tool/timeline-tool/TimeLineTool';
 import { BaseScene } from '../../base/BaseScene';
 import { AudioManager } from '../../ta/tool/AudioManager';
-import { CocosAnimationMultiTool } from '../../ta/tool/cocos-animation-tool/CocosAnimationMultiTool';
 import { AudioClipsEnum } from '../vo/enum/SoundMap';
 const { ccclass, property } = _decorator;
 
 @ccclass('PrizePredictionView')
 export class PrizePredictionView extends BaseScene {
-    @property(CocosAnimationMultiTool)
-    private anim: CocosAnimationMultiTool;
+    @property(TimeLineTool) private anim: TimeLineTool | null = null;
 
+    private sounds: AudioClipsEnum[] = [
+        AudioClipsEnum.PrizePrediction01,
+        AudioClipsEnum.PrizePrediction02,
+        AudioClipsEnum.PrizePrediction03
+    ];
     private callBack: Function = null;
     onLoad() {
         super.onLoad();
@@ -31,11 +35,12 @@ export class PrizePredictionView extends BaseScene {
     public playVertical(cb: Function): void {
         this.playSound();
         this.callBack = cb;
-        this.anim.OnPlay(0, () => this.onFinish());
+        this.anim.play('Show', () => this.onFinish());
     }
 
     private playSound() {
-        AudioManager.Instance.play(AudioClipsEnum.PrizePrediction);
+        let index = Math.floor(Math.random() * (this.sounds.length - 0.1)); //prevent random number equal 1 will out of range
+        AudioManager.Instance.play(this.sounds[index]);
     }
 
     public stop() {
