603b25d2f09a5a78e7426abfc3f14d8801db2630
shiunrujan <shiunrujan@gmail.com>
2025-03-07 17:09:29 +0800

    fix: IG-644 Omni廳遊戲，選完座位進入遊戲Loading頁面時，有可能於Loading卡住
    註: 主因是init在更新jackpotTypeObj到Set畫面上的Value的過程中，如果有玩家拉走(單一)Grand or Major，這時更新的notification會把init的jackpotTypeObj覆蓋，導致Grand Major在顯示時，會漏一筆資料最終報錯卡住。

--- assets/src/sgv3/command/jackpot/JackpotPoolCommand.ts
+++ assets/src/sgv3/command/jackpot/JackpotPoolCommand.ts
@@ -1,5 +1,6 @@
 import { JackpotPoolProxy } from '../../proxy/JackpotPoolProxy';
 import { JackpotTypeObj } from '../../vo/jackpot/JackpotTypeObj';
+import { JackpotPoolObj } from 'src/sgv3/vo/jackpot/JackpotPoolObj';
 
 export class JackpotPoolCommand extends puremvc.SimpleCommand {
     public static readonly NAME: string = 'jackpotPoolNotify';
@@ -7,12 +8,24 @@ export class JackpotPoolCommand extends puremvc.SimpleCommand {
 
     public execute(notification: puremvc.INotification): void {
         let jackpotTypeObj: JackpotTypeObj = notification.getBody();
+        let jackpotPoolObj: JackpotPoolObj[] = [];
         this.jackpotPoolProxy = this.getJackpotPoolProxy();
 
         if (jackpotTypeObj.typeItems.length > 3) {
             // 有含mini的是初始池
             this.jackpotPoolProxy.jackpotTypeObjInit = jackpotTypeObj;
         }
+
+        if (this.jackpotPoolProxy.jackpotTypeObj.typeItems != null) {
+            this.jackpotPoolProxy.jackpotTypeObj.typeItems.forEach((curItem) => {
+                const target = jackpotTypeObj.typeItems.find((newItem) => newItem.poolName === curItem.poolName);
+                if (target) {
+                    jackpotPoolObj.push(target);
+                }
+            }); 
+            jackpotTypeObj.typeItems = jackpotPoolObj;
+        }
+
         this.jackpotPoolProxy.jackpotTypeObj = jackpotTypeObj;
     }
 
